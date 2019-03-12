ef6ea7ba88c2c613a3a29ad83a5d8ee1e1a60cbf
niss <niss>
2019-03-12 17:10:09 +0100

    updated commented out api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   api: 'https://reviewitapi.azurewebsites.net/api/',
-  //api: 'http://localhost:2529/api/',
+  //api: 'https://localhost:44362/api/',
   blob: 'https://reviewitblob2.blob.core.windows.net/reviewitblob2/'
 };
 
